6caa054e70c6eefc8e3b3837385c1c9397dec201
Rodney Duranty <rodneyy.durantyy@icloud.com>
2024-05-22 14:17:39 +0200

    Create Login.py

---
+++ pages/Login.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+good_password = "1234"
+
+password = st.text_input("tapper votre mot de passe")
+
+
+if good_password == password :
+    st.write("Accès Autorisé")
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)
+
+elif password == "0000":
+    st.write("mot de passe incorrect")
+else:
+    st.write("accès refusé")
+st.write(password)
+
+
