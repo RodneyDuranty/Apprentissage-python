65ff3457757e0364394adbf38fe9ae36788ee4a2
Rodney Duranty <rodneyy.durantyy@icloud.com>
2024-05-22 12:48:08 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+st.title ("helle world ")
+
+# Tyoe de données : entier naturel et les float 
+
+variable_1 = 10
+st.write(variable_1)
+# multiplication 
+st.write(variable_1*2)
+# division 
+st.write(variable_1/2)
+#affichage du type de variable 
+#st.write(type(variable_1/2))
+
+st.write(variable_1//2)
+# puissance 
+st.write(variable_1**2)
+
+#Type de données : string (chaine de carractere )
+variable_2 = " hello world "
+#st.write(type(variable_2))
+#Multiolication de chaine de carractére 
+st.write (variable_2*2)
+#adition de chaine de carractére
+st.write (variable_2+ "!")
