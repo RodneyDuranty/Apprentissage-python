b9ed5a59e807f1f48ceea78fa3d45278070fae20
Rodney Duranty <rodneyy.durantyy@icloud.com>
2024-05-23 23:01:19 +0200

    radio questionnaire

--- pages/Questionnaire.py
+++ pages/Questionnaire.py
@@ -21,3 +21,14 @@ title = st.text_input("Adresse postal", "n° et voies")
 title = st.text_input("complément d'adresse", "porte")
 
 title = st.text_input("Code postal", "75000")
+
+
+genre = st.radio(
+    "Situation :",
+    [":red[en couple]", "célibataire", "marié:"],
+    captions = ["", "", ""])
+
+if genre == ":red[en couple]":
+    st.write("You selected en couple.")
+else:
+    st.write("You didn't select en couple.")
