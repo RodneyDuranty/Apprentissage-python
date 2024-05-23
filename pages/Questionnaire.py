7aa473589d6ba6d76ea789735dfed36da77697b7
Rodney Duranty <rodneyy.durantyy@icloud.com>
2024-05-23 21:47:19 +0200

    Update Questionnaire.py

--- pages/Questionnaire.py
+++ pages/Questionnaire.py
@@ -5,3 +5,7 @@ st.title (" Questionnaire ")
 st.sidebar.image('photo.webp', caption='Rodney Duranty')
 
 title = st.text_input("Nom", "")
+
+title = st.text_input("Prénom", "")
+
+age = st.slider("How old are you?", 0, 130 )
